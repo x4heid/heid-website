c19cb2ffdb453f9ffbd3d3a7b60ebc6440d9a175
Rafael Heid <4heid@informatik.uni-hamburg.de>
2017-03-05 22:12:06 +0100

    Changed: Jquery to regular JS; Worksample-overlay all centered now

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,22 @@
-$(document).ready(function() {
-  $(".work-sample img").click(function(){
-    $(this).next().css("display", "flex");
-  });
+/*
+  Eventhandling for Overlays of work-samples
+*/
+var worksampleimages = document.getElementsByClassName('work-sample-picture');
+var worksampleoverlays = document.getElementsByClassName('work-sample-overlay');
 
-  $(".work-sample-overlay").click(function(){
-    $(this).hide();
-  });
-});
+var showOverlay = function() {
+  this.nextElementSibling.style.display = 'flex';
+}
+
+var hideOverlay = function() {
+  this.style.display = 'none';
+}
+
+console.log(worksampleimages.length);
+for(var i=0; i<worksampleimages.length; i++){
+  worksampleimages[i].addEventListener('mousedown', showOverlay);
+}
+
+for(var i=0; i<worksampleoverlays.length; i++){
+  worksampleoverlays[i].addEventListener('mousedown', hideOverlay);
+}
